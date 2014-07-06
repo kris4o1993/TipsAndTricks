c1194de08b1d57076d79c098fe54d933b09fe078
Kristiyan Petrov <kris4o1993@gmail.com>
2014-07-06 14:31:55 +0300

    array.map

--- pure-js.js
+++ pure-js.js
@@ -92,3 +92,25 @@ for (var i = 0; i < count; i++) {
 
     tagCloud.appendChild(item);
 }
+
+
+//------------------------------------------------------------------------------------------//
+
+//How to use Array.map
+
+// Define the callback function.
+function AreaOfCircle(radius) {
+    var area = Math.PI * (radius * radius);
+    return area.toFixed(0);
+}
+
+// Create an array.
+var radii = [10, 20, 30];
+
+// Get the areas from the radii.
+var areas = radii.map(AreaOfCircle);
+
+document.write(areas);
+
+// Output:
+// 314,1257,2827
