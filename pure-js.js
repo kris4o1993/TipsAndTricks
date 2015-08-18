aa7603f3880b6f41bb7de53241324ef059a06a39
Kristiyan Petrov <kris4o1993@gmail.com>
2015-08-18 14:42:25 +0300

    Update pure-js.js

--- pure-js.js
+++ pure-js.js
@@ -126,11 +126,11 @@ var sum = marks.reduce(function(a, b) { return a + b });
 
 // getting unique GUID id
 function getUniqueId() {
-                function s4() {
-                    return Math.floor((1 + Math.random()) * 0x10000)
-                      .toString(16)
-                      .substring(1);
-                }
-                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                  s4() + '-' + s4() + s4() + s4();
-        }
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+}
