2571f6d64ae5fc1268905613b7bbe8a2ad904b2a
Nikolay Yonchev <nikolay.yonchev@gmail.com>
2016-09-20 10:50:30 +0300

    Encapsulation and Chaining

---
+++ Encapsulation and chaining.js
@@ -0,0 +1,28 @@
+/** 
+ * param is private for the instance
+ * Functions sub, add and print are accessible by the instance
+ * Functions addDigit, subDigit and printDigit cannot be accessed directly
+ * Functions add, sub and print are chained
+ */
+ 
+function Calculator(p) {
+	var param = p;
+	var printDigit = function ()
+	{
+		console.log("Result is", param); 
+		return this;
+	};
+	this.print = printDigit;
+	var addDigit = function (digit)
+	{
+		param = param + digit;
+		return this;
+	};
+	this.add = addDigit;
+	var subDigit = function (digit)
+	{
+		param = param - digit;
+		return this;
+	}
+	this.sub = subDigit;
+}
