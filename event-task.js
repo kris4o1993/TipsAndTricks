b75381573058ffaa374066a531a43e5df2568a7e
Kristiyan Petrov <kris4o1993@gmail.com>
2016-11-04 14:17:19 +0200

    Create event-task.js

---
+++ event-task.js
@@ -0,0 +1,25 @@
+function EventHandler() {
+    this.events = {};
+}
+
+EventHandler.prototype.addEvent = function(name, callback) {
+    if(!this.events['name']) {
+        this.events = [callback];
+    } else {
+        this.events['name'].push(callback);
+    }
+}
+
+EventHandler.prototype.removeEvent = function(name) {
+    if(this.events['name']) {
+        delete this.events['name'];
+    }
+}
+
+EventHandler.prototype.executeEvent = function(name) {
+    if(this.events['name']) {
+        this.events['name'].forEach(function(element) {
+            element();
+        }, this);
+    }
+}
